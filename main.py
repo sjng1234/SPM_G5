7a3a2786644c0cb745246ade9f6d64a1eb34e647
Ng Shen Jie <ng.shen.jie1999@gmail.com>
2021-10-11 14:16:26 +0800

    Linking to local mysql database + build a template CRUD

--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
-# configuration
-DEBUG = True
+from sqlalchemy.sql.schema import Column
 
 # instantiate the app
 app = Flask(__name__)
@@ -18,19 +16,18 @@ db = SQLAlchemy(app)
 # envConfig = dotenv_values(".env")['TEMP']
 
 # enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app)
 
 # Model
 class Todo(db.Model):
     __tablename__ = "todos"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20))
-    todo_description = db.Column(db.String(100))
+    id = Column(db.Integer, primary_key=True)
+    title = Column(db.String(20))
+    todo_description = Column(db.String(100))
 
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
+    __mapper_args__ = {
+        'polymorphic_identity': 'todoitem'
+    }
 
     def __init__(self, title, todo_description):
         self.title = title
@@ -42,13 +39,71 @@ class Todo(db.Model):
 
 db.create_all()
 
-
 # sanity check route
 @app.route('/', methods=['GET'])
 def Welcome():
-    # print(envConfig)
     return jsonify('Successful! Linked to Flask APP!')
 
+@app.route('/insertToDo',methods = ['POST'])
+def insert():
+    if request.content_type == 'application/json':
+        post_data = request.get_json()
+        title = post_data.get('title')
+        desc = post_data.get('todo_description')
+        new_to_do = Todo(title,desc)
+        db.session.add(new_to_do)
+        db.session.commit()
+        return jsonify("Successfully posted!")
+    return jsonify("Oops something went wrong!")
 
+@app.route('/getAll',methods = ['GET'])
+def return_Todos():
+    all_books = db.session.query(Todo.id,Todo.title,Todo.todo_description).all()
+    data = [dict(i) for i in all_books]
+    return jsonify(data)
+
+@app.route('/get/<id>',methods = ['GET'])
+def return_Todo_Item(id):
+    try:
+        book = db.session.query(Todo.id,Todo.title,Todo.todo_description).filter(Todo.id == id).first()
+        return jsonify(dict(book))
+    except Exception:
+        return jsonify({
+            "message": "ToDo ID not found in database."
+        }), 404
+
+@app.route('/delete/<id>', methods=['DELETE'])
+def delete_Todo_Item(id):
+    try:
+        record = db.session.query(Todo).get(id)
+        db.session.delete(record)
+        db.session.commit()
+        return jsonify('Deleted')
+    except Exception:
+        return jsonify({
+            "message": "ToDo ID not found in database."
+        }), 404
+
+@app.route('/update/<id>',methods=['PUT'])
+def update_item(id):
+    try:
+        record = db.session.query(Todo).get(id)
+        if request.content_type == 'application/json':
+            put_data = request.get_json()
+            title = put_data.get('title')
+            desc = put_data.get('todo_description')
+            if 'id' in put_data:
+                print(1)
+                record.id = put_data.get('id')
+            record.title = title
+            record.todo_description = desc
+            db.session.commit()
+            return jsonify('Updated!')
+        else:
+            raise Exception
+    except Exception:
+        return jsonify({
+            "message": "Enter Valid JSON request body or a valid id for database"
+        }),404
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
