764f978f21f235dec0890c6ca1cc91b9849b83cd
Adrian <adrian.poh.ey@gmail.com>
2021-10-21 17:47:32 +0800

    create COURSES table on your localhost

--- create_table_script.sql
+++ create_table_script.sql
@@ -6,3 +6,8 @@ CREATE TABLE IF NOT EXISTS TODOS (
 id INT PRIMARY KEY AUTO_INCREMENT, 
 title varchar(20), 
 todo_description varchar(100));
+
+CREATE TABLE IF NOT EXISTS COURSES (
+course_id varchar(20) PRIMARY KEY, 
+title varchar(20), 
+course_description varchar(100));
