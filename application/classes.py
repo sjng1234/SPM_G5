3c072d2edacdb617ec8b0c65f84449467ce1ae5b
Adrian <adrian.poh.ey@gmail.com>
2021-10-27 20:43:46 +0800

    Sprint2 / Basic CRUD for classes complete

--- application/classes.py
+++ application/classes.py
@@ -23,19 +23,51 @@ def insert():
         return jsonify("Something went wrong!")
     except Exception:
         return jsonify({
-            "Error Message": "An error has occured when adding class, please try again"
+            "Error Message": "An error has occurred when adding class, please try again"
         }), 404
 
+# Get All
 @classes.route("/getAll", methods=["GET"])
 def getAll():
     all_books = Class.query.all()
     data = [i.to_dict() for i in all_books]
     return jsonify(data)
 
-@classes.route("/updateClass/<course_id>/<class_id>", methods=["GET"])
-def update_class_detail(course_id, class_id):
+# Get One
+@classes.route("/getOne/<courid>/<classid>", methods=["GET"])
+def get_classes_from_course(courid, classid):
     try:
-        record = Class.query.get((course_id, class_id))
-        return jsonify(record)
+        record = Class.query.filter_by(course_id = courid, class_id = classid)
+        result = [i.to_dict() for i in record]
+        return jsonify(result)
     except:
-        return "Error"
+        return jsonify({
+            "Error Message": "Course is not found, thus there are no registered classes"
+        }), 404
+
+# UPDATE
+@classes.route("/updateClass/<courid>/<classid>", methods=["PUT"])
+def update_class_detail(courid, classid):
+    try:
+        record = Class.query.filter_by(course_id = courid, class_id = classid).first()
+        if request.content_type == "application/json":
+            put_data = request.get_json()
+            creator_id = put_data.get("class_creator_id")
+            size = put_data.get("class_size")
+            end = put_data.get("end_datetime")
+            start = put_data.get("start_datetime")
+            tid = put_data.get("trainer_id")
+
+            setattr(record, "class_creator_id", creator_id)
+            setattr(record, "class_size", size)
+            setattr(record, "end_datetime", end)
+            setattr(record, "start_datetime", start)
+            setattr(record, "trainer_id", tid)
+
+            db.session.commit()
+            return jsonify("Successful update of class content!")
+        return jsonify("Something is wrong with the JSON script!")
+    except Exception:
+        return jsonify({
+            "message": "Enter Valid JSON request body or a valid id for database"
+        }),404
