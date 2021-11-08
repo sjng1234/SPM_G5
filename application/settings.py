a0aa6976e6dfb3c87e000a974f0866c88157f685
justincheu <cheuzhaoyi@gmail.com>
2021-11-08 14:04:12 +0800

    Flagship Sprint/ Trainer Route Updates

--- application/settings.py
+++ application/settings.py
@@ -11,19 +11,19 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/SPM"
 
 # # for windows
-# SQLALCHEMY_DATABASE_URI = "mysql://root:@localhost:3306/SPM"
+SQLALCHEMY_DATABASE_URI = "mysql://root:@localhost:3306/SPM"
 
 # To Test on Cloud SQL
-CLOUD_DB_USERNAME = os.environ.get('CLOUD_DB_USERNAME')
-CLOUD_DB_PASSWORD = os.environ.get('CLOUD_DB_PASSWORD')
-CLOUD_DB_HOST = os.environ.get('CLOUD_DB_HOST')
-CLOUD_DB_NAME = os.environ.get('CLOUD_DB_NAME')
+# CLOUD_DB_USERNAME = os.environ.get('CLOUD_DB_USERNAME')
+# CLOUD_DB_PASSWORD = os.environ.get('CLOUD_DB_PASSWORD')
+# CLOUD_DB_HOST = os.environ.get('CLOUD_DB_HOST')
+# CLOUD_DB_NAME = os.environ.get('CLOUD_DB_NAME')
 
-SQLALCHEMY_DATABASE_URI = sqlalchemy.engine.url.URL.create(
-    drivername = "mysql+pymysql",
-    username = CLOUD_DB_USERNAME,  
-    password = CLOUD_DB_PASSWORD,  # e.g. "my-database-password"
-    host = CLOUD_DB_HOST,  # e.g. "127.0.0.1"
-    database = CLOUD_DB_NAME,  # e.g. "my-database-name"
-)
+# SQLALCHEMY_DATABASE_URI = sqlalchemy.engine.url.URL.create(
+#     drivername = "mysql+pymysql",
+#     username = CLOUD_DB_USERNAME,  
+#     password = CLOUD_DB_PASSWORD,  # e.g. "my-database-password"
+#     host = CLOUD_DB_HOST,  # e.g. "127.0.0.1"
+#     database = CLOUD_DB_NAME,  # e.g. "my-database-name"
+# )
 
